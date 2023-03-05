d9f83e82562c81095af8d60f570278bcc9f42d30
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2023-03-05 14:45:35 +0200

    4

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,22 @@
-
+import PropTypes from 'prop-types';
+import { StatisticsList } from "./Statistics.styled"
 
 export const Statistics = ({ good, neutral, bad, total, positivePercentage}) => {
     return (
-        <ul>
+        <StatisticsList>
           <li>Good: {good}</li>
           <li>Neutral: {neutral}</li>
           <li>Bad: {bad}</li>
           <li>Total: {total}</li>
           <li>PositivePercentage: {positivePercentage}%</li>
-        </ul>
+        </StatisticsList>
     )
 };
 
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
